e26687969ef6bedc4b0cc9f3dba12b82a94508a3
vincenzomercadante <vincenzo.mercadante0@outlook.it>
2024-03-20 16:37:51 +0100

    fix: template header, footer & comments

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,6 @@
 <footer class="bg-dark text-white">
-  <div class="container py-4">
-    &copy; {{ env('APP_AUTHOR', 'Tiziano Nicolai') }} {{ now()->year }}
-  </div>
+    <div class="container py-4">
+        {{-- footer info --}}
+        &copy; {{ env('APP_AUTHOR', 'Vincenzo Mercadante') }} {{ now()->year }}
+    </div>
 </footer>
